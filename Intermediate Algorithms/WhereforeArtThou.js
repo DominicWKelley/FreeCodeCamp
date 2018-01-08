7f3c7c97f67561c580d454a145586148b4a3404d
DominicWKelley <kelleywebtech@gmail.com>
2018-01-07 23:02:51 -0800

    Romeo

--- Intermediate Algorithms/WhereforeArtThou.js
+++ Intermediate Algorithms/WhereforeArtThou.js
@@ -3,7 +3,14 @@ function whatIsInAName(collection, source) {
   // What's in a name?
   var arr = [];
   // Only change code below this line
-  
+  arr = collection.filter(function(obj){
+    for (var i in source){
+      if (source[i] != obj[i]){
+        return false;
+      }
+    }
+    return true;
+  });
   
   // Only change code above this line
   return arr;
