9fa1f71604b866007700b5b70678c3c8cce56f87
DominicWKelley <kelleywebtech@gmail.com>
2018-01-09 23:48:37 -0800

    Pig Latin Done

--- Intermediate Algorithms/PigLatin.js
+++ Intermediate Algorithms/PigLatin.js
@@ -1,7 +1,24 @@
 
 function translatePigLatin(str) {
+  var aeiou = ['a', 'e', 'i', 'o', 'u'];
+  var pLatin = str.split('');
+  if (aeiou.includes(str[0])){
+    return str += 'way';
+  }
+  else {
+    for (var i = 0; i<str.length; i++){
+      if (!aeiou.includes(str[i])){
+        pLatin.push(pLatin.shift());
+      } 
+      else {
+        pLatin.push('ay');
+        return pLatin.join('');
+      }
+    
+    }
   
-  return str;
+  }
+  
 }
 
 translatePigLatin("consonant");
