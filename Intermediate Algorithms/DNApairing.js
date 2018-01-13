d2a8823207ba42d87b56afe5d9e5bd065f6ee5b0
DominicWKelley <kelleywebtech@gmail.com>
2018-01-12 18:40:07 -0800

    DNA Done

--- Intermediate Algorithms/DNApairing.js
+++ Intermediate Algorithms/DNApairing.js
@@ -1,1 +1,20 @@
-sd
+
+function pairElement(str) {
+  var pairs = [],
+      DNA = str.split('');
+  for (var i=0; i<DNA.length; i++){
+    if (DNA[i] == 'A') {
+      pairs.push(['A','T']);
+    } else if (DNA[i] == 'T') {
+      pairs.push(['T','A']);
+    } else if (DNA[i] == 'G') {
+      pairs.push(['G','C']);
+    } else if (DNA[i] == 'C') {
+      pairs.push(['C','G']);
+    }
+  }
+  console.log(pairs);
+  return pairs;
+}
+
+pairElement("GCG");
