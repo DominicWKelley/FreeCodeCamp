70f981b08ad0d80c77d4bd357afb7874f37caa07
DominicWKelley <kelleywebtech@gmail.com>
2018-01-17 16:12:58 -0800

    MissingLetters complete

--- Intermediate Algorithms/MissingLetters.js
+++ Intermediate Algorithms/MissingLetters.js
@@ -1,6 +1,19 @@
 
 function fearNotLetter(str) {
-  return stmyArr;
+  var missing,
+      charCode = [];
+  
+  for (var i = 0; i < str.length; i++) {
+    charCode.push(str.charCodeAt(i));
+  }
+  
+  for (var j = 0; j < charCode.length - 1; j++) {
+    if ((charCode[j] + 1) != charCode[j+1]) {
+      missing = String.fromCharCode(charCode[j] + 1);
+    }
+  }
+  
+  return missing;
 }
 
 fearNotLetter("abce");
